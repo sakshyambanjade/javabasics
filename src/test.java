267a0267a861fc04b5c6461b3134094e4fda59dd
Your Name <sakshyambanjade@gmail.com>
2024-06-23 12:04:19 +0545

    added the java folder

---
+++ src/test.java
@@ -0,0 +1,10 @@
+class test {
+    public static void main(String arr[]){
+        int a = 10 ;
+        String name = "sakshyam";
+        boolean kishar = true;
+       System.out.println(a);
+       System.out.println(name);
+       System.out.println(kishar);
+    }
+}
